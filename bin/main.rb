5d874d3f10242d39dad355572af18de12e61f2cf
SevlaMare <sevilamare@gmail.com>
2020-04-14 15:50:57 -0300

    first draft

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,22 @@
 #!/usr/bin/env ruby
 
+require 'nokogiri'
+require 'open-uri'
+
+# Fetch and parse HTML document
+doc = Nokogiri::HTML(open('https://www.indeed.com/jobs?q=full+stack&l='))
+
+# get raw data
+puts 'Search results for Full Stack'
+doc.css('div#searchCountPages').each do |link|
+  p link.content
+end
+
+puts 'Income offer'
+doc.css('.salaryText').each do |link|
+  p link.content
+end
+
+# clean data
+
+# display data
