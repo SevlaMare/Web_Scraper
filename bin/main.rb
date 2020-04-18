3c3d07fb6ad10ddac76a3bd925bf9d53d88f5ea6
SevlaMare <sevilamare@gmail.com>
2020-04-17 21:36:03 -0300

    Add main file to call all methods

---
+++ bin/main.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'open-uri'
+require 'csv'
+require_relative '../lib/metadata.rb'
+require_relative '../lib/engine.rb'
+
+def show_all(page = 1, export = true, search = 'ruby')
+  scrap = Engine.new
+  page = scrap.scan_now(page, search)
+
+  # --- STORE DATA ---
+  scrap.get_it(page, 'title')
+  scrap.get_it(page, 'location')
+  scrap.get_it(page, 'salary')
+  scrap.get_it(page, 'content')
+
+  # --- CLEAN DATA ---
+  scrap.remove_duplicates
+
+  # --- DISPLAY ---
+  scrap.display('title')
+  scrap.display('location')
+  scrap.display('salary')
+  scrap.display('content')
+
+  # --- EXPORT CSV ---
+  scrap.export if export
+end
+
+# page_number, export_to_csv, search term
+show_all(3, true, 'ruby rails')
