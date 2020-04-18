6e239f7cfecba0c72742f583dece2589364aa456
SevlaMare <sevilamare@gmail.com>
2020-04-17 21:33:12 -0300

    Add get_it to capture raw data

--- lib/engine.rb
+++ lib/engine.rb
@@ -5,4 +5,17 @@ class Engine < Metadata
     link = "https://www.simplyhired.com/search?q=#{term}&pn=#{page}"
     Nokogiri::HTML(URI(link).open)
   end
+  
+  # --- GET RAW DATA ---
+  def get_it(site, argx)
+    key = @title if argx == 'title'
+    key = @location if argx == 'location'
+    key = @salary if argx == 'salary'
+    key = @content if argx == 'content'
+
+    site.css(@map_class[argx]).each do |card|
+      key << card.content
+    end
+    key
+  end
 end
